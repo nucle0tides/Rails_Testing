360a6f5a3d49083aad1314776bb8996cafaad8ae
Gabby Ortman <gabrielle.ortman@cerner.com>
2016-06-01 11:14:22 -0500

    add user following, broke user_pages_spec tests whoops

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -58,6 +58,55 @@ describe "User pages" do
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
   end
 
   describe "signup page" do
@@ -65,7 +114,6 @@ describe "User pages" do
 
       it { should have_content('Sign up') }
       it { should have_title(full_title('Sign up')) }
-    end
 
     describe "signup" do
 
@@ -139,22 +187,5 @@ describe "User pages" do
       specify { expect(user.reload.email).to eq new_email }
     end
   end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
-
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-
-    describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
-      it { should have_content(user.microposts.count) }
-    end
-  end
-  
+end 
 end
